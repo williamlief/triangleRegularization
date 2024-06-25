5c7a591b55da0d114a9b3c9f4a874b2a454de92b
Lief <williamlief@gmail.com>
2024-06-25 13:09:20 -0700

    Added fn to plot spring network,

--- R/plot.R
+++ R/plot.R
@@ -2,7 +2,7 @@
 #'
 #' Distance between nodes is not scaled to distance values - it can't be since
 #' we want to show invalid triangles! Invalid triangles are highlighted in
-#' red.Since edges are shared, a valid triangle may have all of its edges
+#' red. Since edges are shared, a valid triangle may have all of its edges
 #' showing as red if all neighboring triangles are invalid.
 #'
 #' @inheritParams learn_polys
@@ -29,8 +29,6 @@
 #' layout <- save_layout(df_edges)
 #' triangle_plot(df_edges, tris, layout = layout)
 #' triangle_plot(df_edges, tris, layout = layout)
-
-
 triangle_plot <- function(df_edges, polys, layout = NULL, tolerance = 0.001, ...) {
 
   bad_polys <- invalid_polys(df_edges, polys)
@@ -55,7 +53,39 @@ triangle_plot <- function(df_edges, polys, layout = NULL, tolerance = 0.001, ...
 
 }
 
-
+#' Create a plot of springs
+#'
+#' @inheritParams learn_spring
+#' @param layout Node layout is non-deterministic. See igraph package
+#'   documentation for more details. You can save a layout using
+#'   `save_layout` to have the same node layout in multiple graphs.
+#' @param ... additional arguments passed to `plot`#' @param ... 
+#'
+#' @return a plot
+#' @export
+#'
+#' @examples
+#' df_edges <-
+#' tidyr::tribble(
+#'   ~node1, ~node2, ~edge, ~weight, ~node1_weight, ~node2_weight,
+#'   "a", "a1", 2, 1, 1, 1,
+#'   "a1", "b1", 1, 1, 1, 1,
+#'   "b1", "b", 2, 1, 1, 1,
+#' )
+#' 
+#' layout <- save_layout(df_edges)
+#' spring_plot(df_edges, layout = layout, main = "input graph")
+spring_plot <- function(df_edges, layout = NULL, ...) {
+  
+  g <- igraph::graph_from_data_frame(df_edges, directed = FALSE)
+  if (is.null(layout)) layout = igraph::layout_nicely(g)
+  
+  plot(g,
+       layout = layout, 
+       edge.label = round(igraph::E(g)$edge, 2),
+       ...)
+  
+}
 
 #' Persistent layout of nodes
 #'
